eeb7823aab8c15ff43a43d5a850047cc9278a425
zeenpoe <zeenpoe@gmail.com>
2026-06-16 09:38:23 +0700

    pembelajaran terakhir kali

---
+++ destructuring.js
@@ -0,0 +1,14 @@
+const data = [
+    "Zeen",
+    17,
+    ["JavaScript", "HTML", "CSS"],
+    "Indonesia"
+];
+
+const [nama, umur, [js, html, css], negara] = data;
+console.log(nama);
+console.log(umur);
+console.log(js);
+console.log(html);
+console.log(css);
+console.log(negara);
